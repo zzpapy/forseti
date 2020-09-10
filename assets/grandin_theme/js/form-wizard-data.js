817d600ce1158a61862b08c98ba957b927e60711
agentom <ryan.soufi@gmail.com>
2020-09-10 09:44:04 +0200

    correction form wizard & submit

--- assets/grandin_theme/js/form-wizard-data.js
+++ assets/grandin_theme/js/form-wizard-data.js
@@ -37,7 +37,7 @@ $(function(){
 			},
 			onFinished: function (event, currentIndex)
 			{
-				alert("Envoyé !");
+				form_2.submit();
 			}
 		}).validate({
 			errorPlacement: function errorPlacement(error, element) { element.before(error); },
@@ -48,45 +48,5 @@ $(function(){
 			}
 		});
 	}
-	
-	$('#datable_1').DataTable({
-		 "bFilter": false,
-		 "bLengthChange": false,
-		 "bPaginate": false,
-		 "bInfo": false,
-		  "footerCallback": function ( row, data, start, end, display ) {
-				var api = this.api(), data;
-	 
-				// Remove the formatting to get integer data for summation
-				var intVal = function ( i ) {
-					return typeof i === 'string' ?
-						i.replace(/[\$,]/g, '')*1 :
-						typeof i === 'number' ?
-							i : 0;
-				};
-	 
-				// Total over all pages
-				var total = api
-					.column( 3 )
-					.data()
-					.reduce( function (a, b) {
-						return intVal(a) + intVal(b);
-					}, 0 );
-	 
-				// Total over this page
-				var pageTotal = api
-					.column( 3, { page: 'current'} )
-					.data()
-					.reduce( function (a, b) {
-						return intVal(a) + intVal(b);
-					}, 0 );
-	 
-				// Update footer
-				$( api.column( 3 ).footer() ).html(
-					'$'+pageTotal
-				);
-			}
-	});
-	
 });
 		
