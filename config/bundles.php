38e7fae54b72369aecdd95d34407eedfcdaf3924
agentom <ryan.soufi@gmail.com>
2020-09-11 12:08:39 +0200

    redirect & reset password

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -37,6 +37,10 @@ class RegistrationController extends AbstractController
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
+        if($this->getUser()){
+            return $this->redirectToRoute('home');
+        }
+
         $user = new User();
         $scm = new Scm();
         $form = $this->createForm(RegistrationFormType::class, $user);
