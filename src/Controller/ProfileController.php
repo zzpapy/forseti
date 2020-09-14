f7533e2b2ffd4d099724449cb8eb5e2a34f31c41
zzpapy <gregory.pace@hotmail.fr>
2020-09-14 09:38:21 +0200

    profile, modif profile no style

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -6,31 +6,69 @@ use App\Entity\User;
 use App\Form\UserAdminType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class ProfileController extends AbstractController
 {
-    /**
-     * @Route("/profile_admin", name="profile_user")
-     */
-    public function index()
+
+    private $slugger;
+
+    public function __construct( SluggerInterface $slugger)
     {
-        return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
-            
-        ]);
+        $this->slugger = $slugger;
     }
+   
     /**
-     * @Route("/profile/{id<\d+>}", name="profile_admin")
+     * @Route("/profile/{id<\d+>}", name="profile")
      */
-    public function profileAdmin(Request $request,User $user)
+    public function index(Request $request,User $user)
     {
-        
+        if($this->getUser()->getId() != $request->get("id")){
+            return $this->redirectToRoute('app_logout');
+        }
         $form = $this->createForm(UserAdminType::class, $user);
         $form->remove('password');
-        return $this->render('profile/profile_admin.html.twig', [
+        $form->add('submit', SubmitType::class, array('label' => 'envoyer'));
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userPicture = $request->files->get("user_admin")["picture"];
+            // dd($request->files->get("user_admin")["picture"]);
+
+            $user->setPicture($this->recordPhoto($userPicture, $user->getEmail()));
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+        return $this->render('profile/profile.html.twig', [
             'controller_name' => 'ProfileController',
             'form' => $form->createView()
         ]);
     }
+
+     /**
+     * @param $photo
+     * @param string $name
+     * @return string $newFilename
+     */
+    private function recordPhoto($photo, string $name)
+    {
+        if ($photo) {
+            $safeFilenameUser = $this->slugger->slug($name);
+            $newFilename = $safeFilenameUser . '-' . uniqid() . '.' . $photo->guessExtension();
+            try {
+                $photo->move(
+                    $this->getParameter('photo'),
+                    $newFilename
+                );
+            } catch (FileException $e) {
+            }
+            return $newFilename;
+        }
+        return null;
+    }
+
 }
