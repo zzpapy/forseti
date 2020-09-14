a055c769cea988780c6a33c1c8be675727b9429b
zzpapy <gregory.pace@hotmail.fr>
2020-09-14 10:42:12 +0200

    modif password in profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -4,12 +4,14 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserAdminType;
+use App\Form\ChangePasswordFormType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class ProfileController extends AbstractController
 {
@@ -24,7 +26,7 @@ class ProfileController extends AbstractController
     /**
      * @Route("/profile/{id<\d+>}", name="profile")
      */
-    public function index(Request $request,User $user)
+    public function index(Request $request,User $user, UserPasswordEncoderInterface $passwordEncoder)
     {
         if($this->getUser()->getId() != $request->get("id")){
             return $this->redirectToRoute('app_logout');
@@ -34,18 +36,34 @@ class ProfileController extends AbstractController
         $form->add('submit', SubmitType::class, array('label' => 'envoyer'));
         $form->handleRequest($request);
 
+        $passForm = $this->createForm(ChangePasswordFormType::class, null);
+        $passForm->add('submit', SubmitType::class, array('label' => 'envoyer'));
+        $passForm->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $userPicture = $request->files->get("user_admin")["picture"];
-            // dd($request->files->get("user_admin")["picture"]);
-
+            
             $user->setPicture($this->recordPhoto($userPicture, $user->getEmail()));
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
         }
+        if ($passForm->isSubmitted() && $passForm->isValid()) {
+            // dd($request->request);
+           
+            $encodedPassword = $passwordEncoder->encodePassword(
+                $user,
+                $passForm->get('plainPassword')->getData()
+            );
+
+            $user->setPassword($encodedPassword);
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
         return $this->render('profile/profile.html.twig', [
             'controller_name' => 'ProfileController',
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'passForm' => $passForm->createView()
         ]);
     }
 
